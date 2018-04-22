1e014435423fc63d29f1ebc8c238d651b196e544
Daniel Marcinkowski <Overgrape@gmail.com>
2018-04-23 00:07:46 +0200

    ss

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pesel
+{
+    class Class1 : Program
+    {
+
+
+
+        public Boolean CheckData(string Pesel, string Data)
+        {
+            char liczbaPesel;
+            char liczbaData;
+            int tmp = 0;
+            bool spr = true;
+            string tmpData = "";
+            for (int i = 0; i < Data.Length; i++)
+            {
+                liczbaPesel = Data[i];
+                if (char.IsDigit(liczbaPesel))
+                {
+                    tmpData = tmpData + liczbaPesel;
+                }
+            }
+            for (int i = 2; i <= 7; i++)
+            {
+
+                liczbaPesel = Pesel[tmp];
+                liczbaData = tmpData[i];
+
+                if (tmp < 5)
+                { tmp = tmp + 1; }
+                if (liczbaPesel == liczbaData)
+                {
+                    spr = true;
+
+                }
+                else spr = false;
+            }
+            return spr;
+        }
+        public Boolean ControlSum(string Pesel)
+        {
+            int[] tabela = new int[] { 1, 3, 7, 9, 1, 3, 7, 9, 1, 3 };
+            int prawidlowaSuma;
+            int wynik = 0;
+            char liczbaPesel;
+            int odejm;
+            bool spr = true;
+            prawidlowaSuma = (int)char.GetNumericValue(Pesel[Pesel.Length - 1]);
+            for (int i = 0; i < 10; i++)
+            {
+                liczbaPesel = Pesel[i];
+                wynik += tabela[i] * (int)char.GetNumericValue(liczbaPesel);
+
+            }
+            odejm = wynik % 10;
+
+
+            if (odejm == 0 && (int)char.GetNumericValue(Pesel[Pesel.Length - 1]) == 0)
+            {
+                return spr;
+            }
+            else if (10 - odejm == prawidlowaSuma)
+            {
+                return spr;
+            }
+            else
+            {
+                spr = false;
+
+                return spr;
+
+            }
+
+
+        }
+        public Boolean CheckDigits(string Pesel) // spr ilości liczb
+        {
+            bool spr = true;
+            if (Pesel.Length == 11)
+            {
+                return spr;
+            }
+            else
+            {
+                spr = false;
+                return spr;
+            }
+        }
+        public Boolean Sex(string Pesel, string Plec)
+        {
+            bool spr = false;
+            char SexLetter;
+            SexLetter = Plec[0];
+            if (SexLetter == 'K' || SexLetter == 'k')
+            {
+                if ((int)char.GetNumericValue(Pesel[Pesel.Length - 2]) % 2 == 0)//Kobieta
+                {
+                    spr = true;
+                    return true;
+
+                }
+            }
+            if (SexLetter == 'M' || SexLetter == 'm')
+            {
+                if ((int)char.GetNumericValue(Pesel[Pesel.Length - 2]) % 2 == 1)//Mężczyzna 
+                {
+                    spr = true;
+
+                    return true;
+
+                }
+            }
+
+            return spr;
+        }
+
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pesel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string Pesel0;
+            string Data;
+            string Plec;
+            bool Odp1;
+            bool Odp2;
+            bool Odp3;
+            bool Odp4;
+
+            Console.WriteLine("Podaj Pesel... ");
+            Pesel0 = Console.ReadLine();
+            Console.WriteLine("Podaj date urodzenia...");
+            Console.WriteLine("Wzór rrrr/mm/dd");
+            Data = Console.ReadLine();
+            Console.WriteLine("Podaj Płęć...");
+            Plec = Console.ReadLine();
+            Class1 check = new Class1();
+
+            //check.CheckPesel(Pesel, Data);
+            Odp1 = check.CheckData(DellSpace(Pesel0), Data);
+            Odp2 = check.CheckDigits(DellSpace(Pesel0));
+            Odp3 = check.Sex(DellSpace(Pesel0), Plec);
+            Odp4 = check.ControlSum(DellSpace(Pesel0));
+            if (check.ControlSum(DellSpace(Pesel0)) == true && check.Sex(DellSpace(Pesel0), Plec) == true && check.CheckDigits(DellSpace(Pesel0)) == true && check.CheckData(DellSpace(Pesel0), Data) == true)
+            {
+                Console.WriteLine("Prawidłowy Pesel");
+            }
+            else
+            {
+                Console.WriteLine("Błędny Pesel");
+
+
+            }
+
+
+            //  PeselNr = Int32.Parse(Pesel);
+
+
+
+
+
+            Console.ReadKey();
+            // Pesel = Int32.Parse(Console.ReadLine());
+        }
+        static String DellSpace(string Pesel0) // usuwanie spacji w peslu
+        {
+            string Pesel = "";
+            /*   Pesel = "";
+               for(int i = 0; i<Pesel0.Length -1; i++)
+               {
+                   if(Pesel0[i] != (char)32)
+                   {
+                       Pesel = Pesel + Pesel0;
+                   }
+               }*/
+            Pesel = Pesel0;
+            return Pesel;
+        }
+    }
+}
